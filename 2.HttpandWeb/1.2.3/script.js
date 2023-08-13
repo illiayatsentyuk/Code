066770119cb7dac93f39bcced26885e849f27c24
illiayatsentyuk <121815889+illiayatsentyuk@users.noreply.github.com>
2023-08-13 11:51:47 +0300

    Update script.js

--- 2.HttpandWeb/1.2.3/script.js
+++ 2.HttpandWeb/1.2.3/script.js
@@ -1,68 +1,91 @@
+function readHttpLikeInput(){
+    var fs = require("fs");
+    var res = "";
+    var buffer = Buffer.alloc ? Buffer.alloc(1) : new Buffer(1);
+    let was10 = 0;
+    for(;;){ 
+        try { fs.readSync(0 /*stdin fd*/, buffer, 0, 1); } catch (e) {break; /* windows */}
+        if(buffer[0] === 10 || buffer[0] === 13) {
+            if (was10 > 10) 
+                break;
+            was10++;
+        } else 
+           was10 = 0;
+        res += new String(buffer);
+    }
 
-// function readHttpLikeInput(){
-//     var fs = require("fs");
-//     var res = "";
-//     var buffer = Buffer.alloc ? Buffer.alloc(1) : new Buffer(1);
-//     let was10 = 0;
-//     for(;;){ 
-//         try { fs.readSync(0 /*stdin fd*/, buffer, 0, 1); } catch (e) {break; /* windows */}
-//         if(buffer[0] === 10 || buffer[0] === 13) {
-//             if (was10 > 10) 
-//                 break;
-//             was10++;
-//         } else 
-//            was10 = 0;
-//         res += new String(buffer);
-//     }
-
-//     return res;
-// }
-// let contents = readHttpLikeInput();
-function outputHttpResponse(statusCode, statusMessage, headers, body) {
-    const date = new Date().toGMTString();
-    console.log(`HTTP/1.1 ${statusCode} ${statusMessage}`);
-    console.log(`Date: ${date}`);
-    console.log(`Server: Apache/2.2.14 (Win32)`);
-    console.log(`Content-Length: ${body.length}`);
-    console.log(`Connection: Closed`);
-    console.log(`Content-Type: text/html; charset=utf-8`);
-    console.log("\n" + body);
+    return res;
 }
    
-function processHttpRequest(method, uri, headers, body) {
-    if(method === 'GET' && uri.startsWith('/sum')){
-        let a = uri.split("?");
-        let b = a[1].split("=");
-        b = b[1].split(",");
-        a = b.map(Number);
-        let summ = 0;
-        for(let i = 0; i < a.length; i++){
-            summ+=a[i];
-        }
-        outputHttpResponse(200,"OK",headers,body);
-        console.log("\n"+summ)
-    }else if(!uri.startsWith("/sum")){
-        outputHttpResponse(404,"Not Fund",headers,body);
+let contents = readHttpLikeInput();
+   
+function outputHttpResponse(statusCode, statusMessage, headers, body,c) {
+    if(statusCode==404){
+        console.log(`HTTP/1.1 ${statusCode} ${statusMessage}
+Date: Sun, 18 Oct 2012 10:36:20 GMT -- тут текущее время
+Server: Apache/2.2.14 (Win32)
+Connection: Closed
+Content-Type: text/html; charset=utf-8
+Content-Length: 2
+    
+not found`);
+    }else if(statusCode==200){
+        console.log(`HTTP/1.1 ${statusCode} ${statusMessage}
+Server: Apache/2.2.14 (Win32)
+Connection: Closed
+Content-Type: text/html; charset=utf-8
+Content-Length: 2
+
+${c}`);
+    }else if(statusCode==400){
+        console.log(`HTTP/1.1 ${statusCode} ${statusMessage}
+Server: Apache/2.2.14 (Win32)
+Connection: Closed
+Content-Type: text/html; charset=utf-8
+Content-Length: 2
+
+${c}`)
+    }
+
+}
+   
+function processHttpRequest($method, $uri, $headers, $body) {
+    let uriStarts = $uri.split("?");
+    let uriNums = uriStarts[1].split("=");
+    uriNums = uriNums[1].split(",")
+    let c = 0;
+    for(let i = 0;i<uriNums.length;i++){
+        c+=parseInt(uriNums[i]);
+    }
+    if(uriStarts[0]!="/sum"){
+       outputHttpResponse(404,"NotFound",$headers,$body);
+    }else if(!uriStarts[1].includes("nums") || $method!="GET"){
+        outputHttpResponse(400,"Bad Request",$headers,$body);
     }else{
-        outputHttpResponse(400,"Bad Request",headers,body);
+        outputHttpResponse(200,"OK",$headers,$body,c);
     }
 }
-   
+
 function parseTcpStringAsHttpRequest(string) { 
-    return { 
-      method: "GET", 
-      uri:"/sum?nums=1,2,3",
-      "headers": {
-          "Host": "student.shpp.me",
-          "Accept": "image/gif, image/jpeg, */*",
-          "Accept-Language": "en-us",
-          "Accept-Encoding": "gzip, deflate",
-          "User-Agent": "Mozilla/4.0",
-          "Content-Length": "35"
-      }, 
-      body :"bookId=12345&author=Tan+Ah+Teck", 
-    }; 
+    const lines = string.split('\n');
+    const [method, uri, protocol] = lines[0].split(' ');
+    const headers = {};
+    let index = 1;
+    while (lines[index] !== '') {
+        const [key, value] = lines[index].split(': ');
+        headers[key] = value;
+        index++;
+    }
+    const requestBody = lines.slice(index + 1).join('');
+    return {
+        method:method,
+        uri:uri,
+        requestBody:requestBody,
+        headers:headers
+    }
 }
 
-http = parseTcpStringAsHttpRequest();
-processHttpRequest(http.method, http.uri, http.headers, http.body);
+   
+http = parseTcpStringAsHttpRequest(contents);
+processHttpRequest(http.method, http.uri, http.headers, http.requestBody);
+   
